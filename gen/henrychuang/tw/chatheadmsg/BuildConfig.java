466888758ce060c614e3badde69a734622ce602d
henrychuangtw <henrychuang.tw@gmail.com>
2014-10-09 11:21:20 +0800

    ChatHead like FB messenger

---
+++ gen/henrychuang/tw/chatheadmsg/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package henrychuang.tw.chatheadmsg;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/henrychuang/tw/chatheadmsg/MyDialog.java
@@ -0,0 +1,86 @@
+package henrychuang.tw.chatheadmsg;
+import henrychuang.tw.chatheadmsg.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MyDialog extends Activity {
+	public static boolean active = false;
+	public static Activity myDialog;
+	
+	EditText edt;
+	Button btn;
+	View top;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		
+		setContentView(R.layout.dialog);
+		
+		edt = (EditText) findViewById(R.id.dialog_edt);
+		btn = (Button) findViewById(R.id.dialog_btn);
+		top = (View)findViewById(R.id.dialog_top);
+				
+		myDialog = MyDialog.this;
+		
+		btn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				String str = edt.getText().toString();
+				if(str.length() > 0){
+//					ChatHeadService.showMsg(MyDialog.this, str);
+					Intent it = new Intent(MyDialog.this, ChatHeadService.class);
+					it.putExtra(Utility.EXTRA_MSG, str);
+					startService(it);
+				}
+			}
+		});
+		
+		
+		top.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				finish();
+			}
+		});
+		
+	}
+		
+	
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		active = true;
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		active = false;
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		active = false;
+	}
+
+	
+	
+}
